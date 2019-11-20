74eca6b4b22938e34fc656a06851582751edb77b
Ero Silva <eronildo.silva@oulook.com>
2019-11-20 14:12:08 -0300

    Adds useStaticQuery to get siteMetadata from graphql.

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,39 +1,37 @@
 import React from 'react';
-import { StaticQuery, graphql } from 'gatsby'
+import { useStaticQuery, graphql } from 'gatsby'
 
-const Profile = () => (
-  <StaticQuery 
-    query={graphql`
-      query MySiteMetadata {
-        site {
-          siteMetadata {
-            title
-            author
-            position
-            description
-          }
-        }
+const Profile = () => {
+  const {
+    site: { 
+      siteMetadata: {
+        title,
+        author,
+        position,
+        description
       }
-    `}
-
-    render={({ 
-      site: { 
-        siteMetadata: {
-          title,
-          author,
-          position,
+    }
+  } = useStaticQuery(graphql`
+    query MySiteMetadata {
+      site {
+        siteMetadata {
+          title
+          author
+          position
           description
         }
       }
-    }) => (
-      <div className="Profile-wrapper">
-        <h1>{title}</h1>
-        <h2>{author}</h2>
-        <h2>{position}</h2>
-        <p>{description}</p>
-      </div>
-    )}
-  />
-)
+    }
+  `);
+
+  return (
+    <div className="Profile-wrapper">
+      <h1>{title}</h1>
+      <h2>{author}</h2>
+      <h2>{position}</h2>
+      <p>{description}</p>
+    </div>
+  );
+}
 
 export default Profile;
