755d563a0caf2d41d255bb672bd49bdf9865f304
Ero Silva <eronildo.silva@oulook.com>
2019-11-20 13:36:27 -0300

    Creates the Profile Component.

---
+++ src/components/Profile/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Profile = () => (
+  <div className="Profile-wrapper">
+    <h1>Ero Silva</h1>
+    <h2>Frontend Developer</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+  </div>
+)
+
+export default Profile;
