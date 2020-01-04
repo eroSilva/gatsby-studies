d206d10efe0330aff0d3060b471f9cf3a3ba6d1a
Erô Silva <eronildo.silva@oulook.com>
2020-01-04 14:45:39 -0300

    Adds structure and data of Social links Component.

---
+++ src/components/SocialLinks/Icons.js
@@ -0,0 +1,11 @@
+import { Github } from 'styled-icons/boxicons-logos/Github'
+import { Twiter } from 'styled-icons/boxicons-logos/Twitter'
+import { LinkedinSquare } from 'styled-icons/boxicons-logos/LinkedinSquare'
+
+const Icons = {
+  Github,
+  Twiter,
+  LinkedinSquare
+}
+
+export default Icons
---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,16 @@
+const links = [
+  {
+    label: 'Github',
+    url: 'https://github.com/eroSilva'
+  },
+  {
+    label: 'Twitter',
+    url: 'https://twitter.com/eroSiilva'
+  },
+  {
+    label: 'Linkedin',
+    url: 'https://www.linkedin.com/in/erosilva/'
+  },
+]
+
+export default links
