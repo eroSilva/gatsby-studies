53a9f2123a4a4552478df3283f99cc8d4fe60374
Erô Silva <eronildo.silva@oulook.com>
2020-01-13 14:49:03 -0300

    Integrates blog post to the project layout.

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,6 +1,11 @@
 import React from "react"
 import { graphql } from "gatsby"
 
+import Layout from "../components/Layout"
+import SEO from "../components/seo"
+import PostItem from "../components/PostItem"
+import Pagination from "../components/Pagination"
+
 export const query = graphql`
   query Posts($slug: String!) {
     markdownRemark(fields: { slug: { eq: $slug } }) {
@@ -17,10 +22,11 @@ const BlogPost = ({ data }) => {
   const post = data.markdownRemark
 
   return (
-    <>
+    <Layout>
+      <SEO title={post.frontmatter.title} />
       <h1>{post.frontmatter.title}</h1>
       <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
-    </>
+    </Layout>
   )
 }
 
