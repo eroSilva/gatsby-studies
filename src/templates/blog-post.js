a126399ccd933178a838a6b32c2bb9b4f5b4c11a
Erô Silva <eronildo.silva@oulook.com>
2020-01-13 14:55:35 -0300

    Adds Post Component Styles.

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -3,8 +3,8 @@ import { graphql } from "gatsby"
 
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
-import PostItem from "../components/PostItem"
-import Pagination from "../components/Pagination"
+
+import * as S from "../components/Post/styled"
 
 export const query = graphql`
   query Posts($slug: String!) {
@@ -12,8 +12,11 @@ export const query = graphql`
       id
       frontmatter {
         title
+        description
+        date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
       }
       html
+      timeToRead
     }
   }
 `
@@ -24,8 +27,16 @@ const BlogPost = ({ data }) => {
   return (
     <Layout>
       <SEO title={post.frontmatter.title} />
-      <h1>{post.frontmatter.title}</h1>
-      <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
+      <S.PostHeader>
+        <S.PostDate>
+          {post.frontmatter.date} | {post.frontmatter.timeToRead} min de leitura
+        </S.PostDate>
+        <S.PostTitle>{post.frontmatter.title}</S.PostTitle>
+        <S.PostDescription>{post.frontmatter.description}</S.PostDescription>
+      </S.PostHeader>
+      <S.MainContent>
+        <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
+      </S.MainContent>
     </Layout>
   )
 }
