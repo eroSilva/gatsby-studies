8e57b807a986a307404a41e33f3f9da0156c0abd
Erô Silva <eronildo.silva@oulook.com>
2020-01-13 12:39:23 -0300

    Adds PostItem content as props.

--- src/components/PostItem/index.js
+++ src/components/PostItem/index.js
@@ -1,22 +1,40 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 import * as S from './styled'
 
-const PostItem = () => (
-  <S.PostItemLink to="/slug/">
+const PostItem = ({
+  slug,
+  category,
+  date,
+  timeToRead,
+  title,
+  description
+}) => (
+  <S.PostItemLink to={slug}>
     <S.PostItemWrapper>
-      <S.PostItemTag background="#47652b">Misc</S.PostItemTag>
+      <S.PostItemTag background="#47652b">{category}</S.PostItemTag>
       <S.PostItemInfo>
-        <S.PostItemDate>13 de Janeiro de 2020 - 4 min de leitura</S.PostItemDate>
+        <S.PostItemDate>{date} - {timeToRead} de leitura</S.PostItemDate>
         <S.PostItemTitle>
-          Estudos sobre GatsbyJS
+          {title}
         </S.PostItemTitle>
         <S.PostItemDescription>
-          Aprendendo a utilizar o GatsbyJS com Willian Justen
+          {description}
         </S.PostItemDescription>
       </S.PostItemInfo>
     </S.PostItemWrapper>
   </S.PostItemLink>
 )
+
+PostItem.propTypes = {
+  slug: PropTypes.string.isRequired,
+  background: PropTypes.string,
+  category: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  timeToRead: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+}
 
 export default PostItem;
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,15 @@ import PostItem from '../components/PostItem'
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <PostItem />
+    <PostItem
+      slug="/about/"
+      background="red"
+      category="Misc"
+      date="13 de Janeiro de 2020"
+      timeToRead="5"
+      title="Estudando desenvolvimento de sites estáticos com GatsbyJS"
+      description="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel doloribus quaerat deleniti eligendi omnis officia nisi veniam iste animi hic obcaecati autem nam, temporibus voluptatum beatae voluptatem optio ratione dolorum?"
+    />
   </Layout>
 )
 
